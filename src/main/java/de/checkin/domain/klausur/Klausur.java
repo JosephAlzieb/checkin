87ca4cbb1d5e6ae9d98898a2c4b99def25e061d8
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-06-01 22:42:31 +0200

    Logik um Freistellung anzupassen. online /Präsenz

--- src/main/java/de/checkin/domain/klausur/Klausur.java
+++ src/main/java/de/checkin/domain/klausur/Klausur.java
@@ -95,3 +95,64 @@ public class Klausur {
     public LocalDate getFreistellungTag() {
         return freistellungTag;
     }
+
+    private void freistellungAnpassen(boolean isOnline, LocalDate tag, LocalTime von, LocalTime bis) {
+        this.freistellungTag = tag;
+        if (isOnline) {
+            freistellungAnpassenFuerOnline(von, bis);
+        }
+        if (!isOnline) {
+            freistellungAnpassenFuerPraesenz(von, bis);
+        }
+    }
+
+    private void freistellungAnpassenFuerPraesenz(LocalTime von, LocalTime bis) {
+        if (von.isAfter(START_ZEIT.plusMinutes(120))) {
+            this.freiStellungVon = von.minusMinutes(120);
+        } else {
+            this.freiStellungVon = START_ZEIT;
+        }
+        if (bis.isBefore(END_ZEIT.minusMinutes(120))) {
+            this.freiStellungBis = bis.plusMinutes(120);
+        } else {
+            this.freiStellungBis = END_ZEIT;
+        }
+    }
+
+    private void freistellungAnpassenFuerOnline(LocalTime von, LocalTime bis) {
+        if (von.isAfter(START_ZEIT.plusMinutes(30))) {
+            this.freiStellungVon = von.minusMinutes(30);
+        } else {
+            this.freiStellungVon = START_ZEIT;
+        }
+
+        if (bis.isAfter(END_ZEIT))
+            bis = END_ZEIT;
+
+        this.freiStellungBis = bis;
+    }
+
+    public boolean isKlausurStornierbar(LocalDate datum) {
+        return datum.isBefore(this.tag);
+    }
+
+    @Override
+    public String toString() {
+        return veranstaltungsName +
+            " (" + tag +
+            ", " + von +
+            " - " + bis +
+            " Uhr)";
+    }
+
+    public String freistellungToString() {
+        return " (" + freiStellungVon +
+            " - " + freiStellungBis +
+            " Uhr)";
+    }
+
+    // Hilfsmethode für html
+    public boolean stornierbar() {
+        return LocalDate.now().isBefore(this.tag);
+    }
+}
